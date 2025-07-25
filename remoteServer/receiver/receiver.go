a4bdd82abacf3a8a85d732c3a22b0d7f5b48b132
parthivsurya <parthivsuryakb@gmail.com>
2025-07-25 20:53:33 +0530

    fioxed cors

--- remoteServer/receiver/receiver.go
+++ remoteServer/receiver/receiver.go
@@ -7,8 +7,14 @@ import (
 	"net/http"
 	//"os/exec"
 )
-
+func enableCORS(w http.ResponseWriter) {
+	// Allow requests from Vite (default port 5173)
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
 func Receive(w http.ResponseWriter, r *http.Request) {
+	enableCORS(w)
 	msg := r.URL.Query().Get("msg")
 	fmt.Fprintf(w, "Received msg: %s\n", msg)
 	fmt.Printf("msg received: %s\n", msg)
