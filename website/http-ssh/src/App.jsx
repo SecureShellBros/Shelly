3a3f412bfe9950dd81e553ff4aad598451f103d0
beppvis <visruthvijind@gmail.com>
2025-07-25 10:52:10 +0530

    base front end

--- website/http-ssh/src/App.jsx
+++ website/http-ssh/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { cors }
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  function sendCommand() {
+
+    let command = document.getElementById("commandInput").value;
+    document.getElementById("commandInput").value = "";
+    let request = new Request(`localhost:6969/command?cmd=${command}`);// might need to changel the command
+    let out;
+    let error;
+    fetch(request)
+      .then((response) => out = response)
+      .catch((exception) => error = exception)
+      ;
+    document.getElementById("output").innerHTML = out;
+    //WARN: might not work :(
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Http-ssh bros</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <textarea id="commandInput" onKeyUp={(key) => { if (key.code == "Enter") sendCommand() }}>
+        </textarea>
+      </div >
+      <div id="output">
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
