cbf9dd97be2a64824b72bfd39b3bb55d5b34dd86
Srisanth <srisanth.p.a@gmail.com>
2025-07-25 23:12:51 +0530

    receiver works and can cmds run in terminal(windows and unix)

--- remoteServer/receiver/receiver.go
+++ remoteServer/receiver/receiver.go
@@ -2,16 +2,39 @@ package receiver
 
 import (
 	"fmt"
+	"io"
 	//"io/ioutil"
-	//"log"
+	"log"
 	"net/http"
-	//"os/exec"
+	"os/exec"
+	"runtime"
+	"strings"
 )
 
 func Receive(w http.ResponseWriter, r *http.Request) {
+	var outputPipe io.ReadCloser
+	var cmdRunning bool
+
 	msg := r.URL.Query().Get("msg")
 	fmt.Fprintf(w, "Received msg: %s\n", msg)
-	fmt.Printf("msg received: %s\n", msg)
+	msg = strings.TrimSpace(msg)
 
-	// for reference , the msg is in 'w' bois
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/C", msg)
+	} else {
+		cmd = exec.Command("sh", "-c", msg)
+	}
+
+	output, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Printf("command failed because of error: %v", err)
+		http.Error(w, "Command failed because of error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	outputPipe = output
+	cmdRunning = true
+
+	// i want to call send( ) here to splice and send
 }
