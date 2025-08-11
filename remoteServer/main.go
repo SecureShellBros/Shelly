023672b60b4464a131f2287d7f7d548c0dfabc31
Srisanth <srisanth.p.a@gmail.com>
2025-08-11 16:48:25 +0530

    get public ip and local ip

--- remoteServer/main.go
+++ remoteServer/main.go
@@ -1,27 +1,54 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
 	"net/http"
+	"strings"
 
 	"http-ssh.rfc/receiver"
+	// "os"
 )
 
-func getOutBoundIP() net.IP {
+func getIPs() (publicIP, localIP string) {
+	resp, err := http.Get("https://api.ipify.org")
+	if err != nil {
+		log.Println("Could not get public IP from ipify:", err)
+		resp, err = http.Get("https://ifconfig.me/ip")
+		if err != nil {
+			log.Fatalf("Fatal: Could not get public IP from any service: %v", err)
+		}
+	}
+	defer resp.Body.Close()
+
+	ipBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Fatal: Could not read response body: %v", err)
+	}
+	publicIP = strings.TrimSpace(string(ipBytes))
+
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
-		log.Fatalf("Could'nt find outbound ip")
+		log.Println("Could not determine local IP:", err)
+		localIP = ""
+	} else {
+		defer conn.Close()
+		localAddr := conn.LocalAddr().(*net.UDPAddr)
+		localIP = localAddr.IP.String()
 	}
-	defer conn.Close()
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-	return localAddr.IP
+
+	return publicIP, localIP
 }
 
 func main() {
 	http.HandleFunc("/data", receiver.TerminalWS)
-	log.Printf("Outbound IP:%s:%s", getOutBoundIP().String(), "8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("server error: %v", err)
+	publicIP, localIP := getIPs()
+	port := "8080"
+	log.Printf("Public IP: %s:%s\n", publicIP, port)
+	log.Printf("Local IP: %s:%s", localIP, port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Server error: %v", err)
 	}
 }
